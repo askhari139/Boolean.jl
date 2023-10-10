0950813dc08110843063e0c53586c18b451ed894
askhari139 <askhari139@gmail.com>
2023-10-10 18:54:58 +0530

    Update scriptWindows.jl

--- scriptWindows.jl
+++ scriptWindows.jl
@@ -4,8 +4,9 @@ topoFiles = map(x->string(x), ARGS)
 println(Threads.nthreads())
 
 Threads.@threads for topoFile in topoFiles
- 	y1 = @elapsed x = bmodel_reps(topoFile; nInit = 100000, nIter = 1000, mode = "Async", stateRep = -1, randSim = false, shubham=true, discrete = false)
-	y2 = @elapsed x = bmodel_reps(topoFile; nInit = 100000, nIter = 1000, mode = "Async", stateRep = -1, randSim = false, shubham=false, discrete = false)
- 	#y2 = @elapsed x = bmodel_reps(topoFile; nInit = 100000, nIter = 1000, mode = "Async", stateRep = 0, randSim = false)
+ 	y1 = @elapsed x = bmodel_reps(topoFile; nInit = 100000, nIter = 1000, mode = "Async", stateRep = -1, shubham=true, nLevels=2)
+	y2 = @elapsed x = bmodel_reps(topoFile; nInit = 100000, nIter = 1000, mode = "Async", stateRep = -1)
+	y3 = @elapsed x = bmodel_reps(topoFile; nInit = 100000, nIter = 1000, mode = "Async", stateRep = 0)
+ 	y1 = @elapsed x = bmodel_reps(topoFile; nInit = 100000, nIter = 1000, mode = "Async", stateRep = -1, shubham=true, nLevels=1, vaibhav=true)
  	println(topoFile, " - ", y1, " seconds, ", y2, " seconds.")
 end
