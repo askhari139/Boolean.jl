cc4fb5b4ba00f56184636251c97979b11376fe9b
Kishore Hari <askhari139@gmail.com>
2024-05-14 03:38:30 -0400

    Update script.jl

--- script.jl
+++ script.jl
@@ -12,7 +12,7 @@ end
 println(Threads.nthreads())
 
 Threads.@threads for topoFile in topoFiles
- 	y1 = @elapsed x = bmodel_reps(topoFile; nInit = 100000, nIter = 1000, mode = "Async", stateRep = -1, randSim=false, shubham = true)
+ 	y1 = @elapsed x = bmodel_reps(topoFile; nInit = 100000, nIter = 1000, mode = "Async", stateRep = -1)
  	# println(topoFile, " - ", y1, " seconds.")
 end
 #for topoFile in topoFiles
